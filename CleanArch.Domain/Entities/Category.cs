900c704f28f72d70dc1caf9bffa1768ab76eb55c
Pedro Ferreira <pedrophpvp12@gmail.com>
2023-12-30 18:57:51 -0300

    feat: criação ValiteDomain na model Category

--- CleanArch.Domain/Entities/Category.cs
+++ CleanArch.Domain/Entities/Category.cs
@@ -1,4 +1,7 @@
-﻿namespace CleanArch.Domain.Entities
+﻿
+using CleanArch.Domain.Validation;
+
+namespace CleanArch.Domain.Entities
 {
     public sealed class Category
     {
@@ -8,12 +11,21 @@
 
         public Category(string name)
         {
-            Name = name;
+            ValiteDomain(name);
         }
 
         public Category(int id, string name)
         {
             Id = id;
+            Name = name;
+        }
+
+        private void ValiteDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Nome inválido. O nome é obrigatório.");
+
+            DomainExceptionValidation.When(name.Length < 3, "Nome muito pequeno. Mínimo 3 caracteres.");
+
             Name = name;
         }
     }
