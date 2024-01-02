cb27f0fa7e2ea0d4e96bfabf3a0c9fc265593424
Pedro Ferreira <pedrophpvp12@gmail.com>
2024-01-01 21:27:51 -0300

    feat: criação interface repository category na camada de domain

--- CleanArch.Domain/Interfaces/ICategoryRepository.cs
+++ CleanArch.Domain/Interfaces/ICategoryRepository.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CleanArch.Domain.Entities;
 
 namespace CleanArch.Domain.Interfaces
 {
-    internal interface ICategoryRepository
+    public interface ICategoryRepository
     {
+        Task<IEnumerable<Category>> GetCategories();
+        Task<Category> GetById(int? id);
+        Task<Category> Create(Category category);
+        Task<Category> Update(Category category);
+        Task<Category> Remove(Category category);
     }
 }
