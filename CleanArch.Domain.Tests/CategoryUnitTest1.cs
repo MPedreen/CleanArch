11d730ca0894251ec9b3bf1a61818467a3f88f45
Pedro Ferreira <pedrophpvp12@gmail.com>
2024-01-01 21:45:25 -0300

    feat: finalização testes de unidade category

--- CleanArch.Domain.Tests/CategoryUnitTest1.cs
+++ CleanArch.Domain.Tests/CategoryUnitTest1.cs
@@ -1,11 +1,51 @@
+using CleanArch.Domain.Entities;
+using FluentAssertions;
+
 namespace CleanArch.Domain.Tests
 {
     public class CategoryUnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Fact(DisplayName = "Create Category With Valid State")]
+        public void CreateCategory_WithValidParameters_ResultObjectValidState()
         {
+            Action action = () => new Category(1, "Category Name ");
+            action.Should()
+                 .NotThrow<CleanArch.Domain.Validation.DomainExceptionValidation>();
+        }
 
+        [Fact]
+        public void CreateCategory_NegativeIdValue_DomainExceptionInvalidId()
+        {
+            Action action = () => new Category(-1, "Category Name ");
+            action.Should()
+                .Throw<CleanArch.Domain.Validation.DomainExceptionValidation>()
+                 .WithMessage("Valor do Id inválido.");
+        }
+
+        [Fact]
+        public void CreateCategory_ShortNameValue_DomainExceptionShortName()
+        {
+            Action action = () => new Category(1, "Ca");
+            action.Should()
+                .Throw<CleanArch.Domain.Validation.DomainExceptionValidation>()
+                   .WithMessage("Nome muito pequeno. Mínimo 3 caracteres.");
+        }
+
+        [Fact]
+        public void CreateCategory_MissingNameValue_DomainExceptionRequiredName()
+        {
+            Action action = () => new Category(1, "");
+            action.Should()
+                .Throw<CleanArch.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Nome inválido. O nome é obrigatório.");
+        }
+
+        [Fact]
+        public void CreateCategory_WithNullNameValue_DomainExceptionInvalidName()
+        {
+            Action action = () => new Category(1, null);
+            action.Should()
+                .Throw<CleanArch.Domain.Validation.DomainExceptionValidation>();
         }
     }
 }
