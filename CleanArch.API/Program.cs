3828cb32a6d10ba710b99816f0d6831e60eccc03
Pedro Ferreira <pedrophpvp12@gmail.com>
2024-02-04 19:29:11 -0300

    feat: registrando na Program AddInfrastructureAPI e configurando appsettings

--- CleanArch.API/Program.cs
+++ CleanArch.API/Program.cs
@@ -1,8 +1,11 @@
+using CleanArch.Infra.IoC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddInfrastructureAPI(builder.Configuration);
 
 var app = builder.Build();
 
