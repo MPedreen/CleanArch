7cf51dc730e8ff1d597c9e4722454b1199ecb8ed
Pedro Ferreira <pedrophpvp12@gmail.com>
2024-02-04 19:44:54 -0300

    feat: criação controller categories e criação do método get

---
+++ CleanArch.API/Controllers/CategoriesController.cs
@@ -0,0 +1,28 @@
+﻿using CleanArch.Application.DTOs;
+using CleanArch.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get()
+        {
+            var categories = await _categoryService.GetCategories();
+
+            if (categories is null)
+                return NotFound("Categories not found.");
+
+            return Ok(categories);
+        }
+    }
+}
