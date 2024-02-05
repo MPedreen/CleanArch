5e4896cc19bdf896f2e6d3298833db41f6c6f13a
Pedro Ferreira <pedrophpvp12@gmail.com>
2024-02-05 20:48:22 -0300

    feat: criação TokenController

---
+++ CleanArch.API/Controllers/TokenController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+
+    }
+}
