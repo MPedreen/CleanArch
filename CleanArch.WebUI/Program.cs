be30325a10cf0be5c63c1c28f05c5b5b1f7bcb23
Pedro Ferreira <pedrophpvp12@gmail.com>
2024-02-15 19:59:22 -0300

    feat: registrando dependência AddInfrastructureAPI

--- CleanArch.WebUI/Program.cs
+++ CleanArch.WebUI/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddInfrastructureAPI(builder.Configuration);
 
 var app = builder.Build();
 
